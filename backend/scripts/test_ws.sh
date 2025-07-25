9ea73788bc24594177ade76b1826d894729b9f56
Kirill Shpin <kirill.shpin@gmail.com>
2025-07-25 02:27:34 -0700

    change websocket endpoint to root
    this is the only endpoint we're planning to expose anyway

--- backend/scripts/test_ws.sh
+++ backend/scripts/test_ws.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Testing websocket connection to localhost:3210/ws in interactive mode."
+echo "Testing websocket connection to localhost:3210 in interactive mode."
 echo "Type your message and press enter to send."
 
-websocat -t ws://localhost:3210/ws
+websocat -t ws://localhost:3210
--- backend/src/server/listener.rs
+++ backend/src/server/listener.rs
@@ -4,7 +4,7 @@ use tokio::net::TcpListener;
 use crate::server::websocket::websocket_handler;
 
 pub async fn start(addr: &str) {
-    let app = Router::new().route("/ws", any(websocket_handler));
+    let app = Router::new().route("/", any(websocket_handler));
 
     tracing::debug!("listening on {}", addr);
 
