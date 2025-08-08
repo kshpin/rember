4ea6161bc24e650dc6d89852d63e9dbcf078b297
Kirill Shpin <kirill.shpin@gmail.com>
2025-08-07 20:35:29 -0700

    add environment variable setup

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -3,8 +3,17 @@ mod server;
 
 #[tokio::main]
 async fn main() {
+    // only for `cargo run` development
+    // in production, the environment variables are set by the compose file
+    dotenvy::dotenv().ok();
+
+    let log_level = std::env::var("LOG_LEVEL")
+        .unwrap_or("info".to_string())
+        .parse::<tracing::Level>()
+        .unwrap_or(tracing::Level::INFO);
+
     tracing_subscriber::fmt()
-        .with_max_level(tracing::Level::DEBUG)
+        .with_max_level(log_level)
         .with_target(false)
         .init();
 
