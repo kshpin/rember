963b9106c491f358c91fceef05bc1792dd94ceb3
Kirill Shpin <kirill.shpin@gmail.com>
2025-08-15 00:03:45 -0700

    move test script to be a client

--- clients/manual/run.sh
+++ clients/manual/run.sh
@@ -3,4 +3,10 @@
 echo "Testing websocket connection to localhost:3210 in interactive mode."
 echo "Type your message and press enter to send."
 
+echo "Try these:"
+echo '{"type": "create_note", "data": {"text": "Hello, world!"}}'
+echo '{"type": "get_notes", "data": {}}'
+
+echo ""
+
 websocat -t ws://localhost:3210
