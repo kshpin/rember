97c598dba0cf20eca606e2e30a0d8d437c4ac506
Kirill Shpin <kirill.shpin@gmail.com>
2025-08-25 01:30:46 -0700

    make TextBox a widget for easy rendering

--- clients/tui/src/renderer.rs
+++ clients/tui/src/renderer.rs
@@ -6,36 +6,36 @@ use ratatui::{
 };
 use std::cmp::{max, min};
 
-use crate::App;
+use crate::{App, TextBox};
 
 impl Widget for &App {
     fn render(self, area: Rect, buf: &mut Buffer) {
         let search_area = Rect::new(0, 0, area.width, 3).clamp(area);
-        self.render_search_bar(&self.search_box.text, search_area, buf);
+        self.search_box.render(search_area, buf);
     }
 }
 
-impl App {
-    fn render_search_bar(&self, text: &str, area: Rect, buf: &mut Buffer) {
+impl Widget for &TextBox {
+    fn render(self, area: Rect, buf: &mut Buffer) {
         let cursor_style = Style::default().bg(Color::White);
         let selection_style = Style::default().bg(Color::LightBlue);
 
-        let selection_range = self.search_box.cursor.selection_anchor.map(|anchor| {
+        let selection_range = self.cursor.selection_anchor.map(|anchor| {
             (
-                min(anchor, self.search_box.cursor.position),
-                max(anchor, self.search_box.cursor.position),
+                min(anchor, self.cursor.position),
+                max(anchor, self.cursor.position),
             )
         });
 
         let mut spans = Vec::new();
 
-        for (i, c) in text.chars().enumerate() {
+        for (i, c) in self.text.chars().enumerate() {
             if let Some((selection_left, selection_right)) = selection_range
                 && i >= selection_left
                 && i < selection_right
             {
                 spans.push(Span::styled(c.to_string(), selection_style));
-            } else if i == self.search_box.cursor.position {
+            } else if i == self.cursor.position {
                 spans.push(Span::styled(c.to_string(), cursor_style));
             } else {
                 spans.push(Span::raw(c.to_string()));
@@ -43,11 +43,11 @@ impl App {
         }
 
         // render cursor if it's at the end of the text
-        if self.search_box.cursor.position == text.len() {
+        if self.cursor.position == self.text.len() {
             spans.push(Span::styled(" ", cursor_style));
         }
 
-        let search_bar = Paragraph::new(Line::from(spans)).block(Block::bordered().title("Search"));
-        search_bar.render(area, buf);
+        let text_box = Paragraph::new(Line::from(spans)).block(Block::bordered().title("Search"));
+        text_box.render(area, buf);
     }
 }
