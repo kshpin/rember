8cce3fb8cc0d80774ca821796f784713fd5d058d
Kirill Shpin <kirill.shpin@gmail.com>
2025-08-25 02:16:42 -0700

    make cursor visible when selecting left

--- clients/tui/src/renderer.rs
+++ clients/tui/src/renderer.rs
@@ -18,7 +18,8 @@ impl Widget for &App {
 impl Widget for &TextBox {
     fn render(self, area: Rect, buf: &mut Buffer) {
         let cursor_style = Style::default().bg(Color::White);
-        let selection_style = Style::default().bg(Color::LightBlue);
+        let selection_style = Style::default().bg(Color::LightGreen);
+        let selection_cursor_style = Style::default().bg(Color::Yellow);
 
         let selection_range = self.cursor.selection_anchor.map(|anchor| {
             (
@@ -28,10 +29,16 @@ impl Widget for &TextBox {
         });
 
         let mut spans = Vec::new();
-
         for (i, c) in self.text.chars().enumerate() {
             if i == self.cursor.position {
-                spans.push(Span::styled(c.to_string(), cursor_style));
+                let style = if let Some((selection_left, _)) = selection_range
+                    && i == selection_left
+                {
+                    selection_cursor_style
+                } else {
+                    cursor_style
+                };
+                spans.push(Span::styled(c.to_string(), style));
             } else if let Some((selection_left, selection_right)) = selection_range
                 && i >= selection_left
                 && i < selection_right
